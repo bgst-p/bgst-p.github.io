3a4ec759a2523b7711acc08ccb57c05ea3f5ceb3
Bagas Satria Pambudi <bagassatriap3@gmail.com>
2023-02-18 21:02:28 +0700

    first up

---
+++ config.js
@@ -0,0 +1,53 @@
+const reso = document.getElementById("reso")
+const pointer = document.getElementById("pointer")
+const explain = document.getElementById("explain")
+const ppimg = document.getElementById("ppimg")
+const ppimg2 = document.getElementById("ppimg2")
+const badge = document.getElementById("badge")
+var x = 0;
+var y = 0;
+
+setInterval(function() {
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+
+    reso.innerHTML = w+" x "+h;
+
+    if (w <= 766.5){
+        ppimg.style.display = "none"
+        ppimg.style.width = "0px"
+        ppimg2.style.display = "block"
+        setTimeout(function(){
+            ppimg2.style.height = "200px"
+        },200)
+    }else{
+        ppimg.style.display = "block"
+        setTimeout(function(){
+            ppimg.style.width = "230px"
+        },200)
+        ppimg2.style.display = "none"
+        ppimg2.style.height = "0px"
+    }
+}, 50);
+
+setInterval(function() {
+    x++;
+    if (x % 2 == 0) {
+        pointer.style.opacity = 0;
+    } else {
+        pointer.style.opacity = 1;
+    }
+
+}, 800);
+
+setInterval(function() {
+    y++;
+    if (y == 18) {
+        y = 0;
+    }
+
+    let text = "'project.' ";
+    let result = text.slice(-y);
+    document.getElementById("project").innerHTML = result;
+
+}, 200);
